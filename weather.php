e5c1dbea6573b9c9a02c7a9c5497d64339f220d2
Pederytter <Pederytter@gmail.com>
2017-02-09 01:22:36 +0100

    Lille update to weatherAPI

---
+++ weather.php
@@ -0,0 +1,49 @@
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Document</title>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+		<script>
+			$(document).ready(function(){
+
+
+				$( "input" ).keyup(function() {
+					var city = $( this ).val();
+					var city = city
+					$( "#city" ).text( city );
+				}).keyup();
+
+
+				$('.jsncall').click(function(){
+					var city = $( this ).val();
+					var city = city.toString();
+					var apiFirst = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + ',dk&appid=9e24f0bb5893a49bb7e40a9bf368bc7c&units=metric';
+					alert(apiFirst);
+					$.getJSON(apiFirst, function(data){
+						console.log(data);
+						var test = data.main.temp;
+						$('#test').html(test);
+						$.each(data,function(key,val){
+							if(json_object.hasOwnProperty('main')){
+								alert(test);
+							};
+						});
+					});
+				});
+
+
+
+
+			});
+		</script>
+	</head>
+	<body>
+
+
+		<input type="text" value="Ingen by"> <br>
+		<p id="city"></p> <br>
+		<div class="jsnCall">output json</div>
+		<br>
+		<p id="test"></p>
+	</body>
+</html>
